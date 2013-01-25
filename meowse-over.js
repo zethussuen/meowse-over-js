f6d230bf3a70c4263b71343cce276cf23e7fc4c0
Zethus Suen <zethus.suen@gmail.com>
2013-01-25 06:59:03 -0500

    pulled in google images json

--- meowse-over.js
+++ meowse-over.js
@@ -8,28 +8,27 @@
 ;(function($) {
   $.fn.meowseOver = function(options){
     var opts = $.extend({}, $.fn.meowseOver.defaults, options);
-    
+    var lolcat = 'https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=lolcat';
+
     return this.each(function(){
       var me = $(this);
       var meowseTitle = "";
       me.hover(function(e){
-        $(this).css('position', 'relative');
         meowseTitle = me.attr('data-title');
+        var rand = Math.floor(Math.random()*101);
+        var url = lolcat + '[' + rand + ']&imgsz=medium&callback=?';
+        var self = this;
         if(meowseTitle.length>0){
-          $(this).append('<div class="meow popover">Meow!</div>');
+          $.getJSON(url, function(data){
+            var src = data.responseData.results[0].tbUrl;
+            var width = data.responseData.results[0].tbWidth;
+            var height = data.responseData.results[0].tbHeight;
+            $(self).append('<div class="meow popover"><img src="' + src + '"></img></div>').css('width', tbWidth).css('height', tbHeight);
+          });
         }
       },function(){
-        me.attr("data-title", meowseTitle);
         $(".meow").remove();
       });
     });
-  };
-  
-  $.fn.meowseOver.defaults = {
-    title: null,
-    xOffset: 0,
-    yOffset: -50,
-    delay: 400,
-    overrideElementTitle: false
   };
 })(window.jQuery);
